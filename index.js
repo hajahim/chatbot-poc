ae0a13f186602d7c7414e885f61cf8b8282fe97a
rrajaona <ravoatra.rajaonarivelo@proximitybbdo.fr>
2019-10-21 17:47:43 +0400

    [Kortex][Chatbot] : Linking chatbot with facebook page

--- index.js
+++ index.js
@@ -20,7 +20,7 @@
  */
 
 'use strict';
-const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
+const PAGE_ACCESS_TOKEN = 'EAADpXta4RXIBAMZB7DlBTyCZA4mZBeoFiDLgy1xcfeRTLZCbkuJLPS7tf0s35PTO7jY7t95XR6YF191rIUlAWNSq3kKewCXGTsKMTJhvtaAK24EKVGx10toEmyftiU8Tcvvc4inzJ1YlB0wJZBiS6Gg9AeRtnRNzZChpXLDLMbIMRy9uLeGzx4';
 // Imports dependencies and set up http server
 const 
   request = require('request'),
@@ -171,7 +171,7 @@ function callSendAPI(sender_psid, response) {
   // Send the HTTP request to the Messenger Platform
   request({
     "uri": "https://graph.facebook.com/v2.6/me/messages",
-    "qs": { "access_token": 'jajahim19' },
+    "qs": { "access_token": PAGE_ACCESS_TOKEN },
     "method": "POST",
     "json": request_body
   }, (err, res, body) => {
